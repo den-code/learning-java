3f836158504af1dc665292768c5e6cb5a0c04246
Denis Avtsin <davtsin@gmail.com>
2019-01-20 12:04:42 +0300

    added logger

---
+++ src/main/java/com/davtsin/jxls/DataHolder.java
@@ -0,0 +1,16 @@
+package com.davtsin.jxls;
+
+public class DataHolder {
+    private int number = 100;
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "DataHolder{" +
+                "number=" + number +
+                '}';
+    }
+}
---
+++ src/main/java/com/davtsin/jxls/JxlsChartTest.java
@@ -0,0 +1,31 @@
+package com.davtsin.jxls;
+
+import org.jxls.common.Context;
+import org.jxls.util.JxlsHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class JxlsChartTest {
+    private static Logger logger = LoggerFactory.getLogger(JxlsChartTest.class);
+
+    public static void main(String[] args) throws IOException {
+
+        DataHolder dataHolder = new DataHolder();
+
+        try (InputStream is = JxlsChartTest.class.getResourceAsStream("chart-sheet.xls")) {
+            try (OutputStream os = new FileOutputStream("target/chart-sheet.xls")) {
+                Context context = new Context();
+                context.putVar("dataHolder", dataHolder);
+
+                logger.debug("DataHolder: {}", context.getVar("dataHolder"));
+
+                JxlsHelper.getInstance().processTemplate(is, os, context);
+            }
+        }
+    }
+}
