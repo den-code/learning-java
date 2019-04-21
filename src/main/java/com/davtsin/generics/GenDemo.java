ab1875d2bbcda349f8ed5ba6c9d1230dee78ba67
Denis Avtsin <davtsin@gmail.com>
2019-04-21 12:28:56 +0300

    generics

---
+++ src/main/java/com/davtsin/generics/GenDemo.java
@@ -0,0 +1,38 @@
+package com.davtsin.generics;
+
+class Gen<T> {
+    T ob;
+
+    Gen(T o) {
+        ob = o;
+    }
+
+    T getOb() {
+        return ob;
+    }
+
+    void showType() {
+        System.out.println("Типом T является " + ob.getClass().getName());
+    }
+}
+
+public class GenDemo {
+    public static void main(String[] args) {
+
+        Gen<Integer> iOb;
+
+        iOb = new Gen<Integer>(88);
+
+        iOb.showType();
+
+        int v = iOb.getOb();
+        System.out.println("Значение: " + v);
+        System.out.println();
+
+        Gen<String> strOb = new Gen<String> ("Тест обобщений");
+        strOb.showType();
+
+        String str = strOb.getOb();
+        System.out.println("Значение: " + str);
+    }
+}
