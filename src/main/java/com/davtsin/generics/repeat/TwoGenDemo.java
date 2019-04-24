0684eb9ae8db45bb9c0417b42d42f10236aa5e35
Denis Avtsin <davtsin@gmail.com>
2019-04-24 21:57:31 +0300

    generics: TwoGenDemo

---
+++ src/main/java/com/davtsin/generics/repeat/TwoGenDemo.java
@@ -0,0 +1,31 @@
+package com.davtsin.generics.repeat;
+
+class TwoGen<T, V> {
+    T ob1;
+    V ob2;
+
+    public TwoGen(T ob1, V ob2) {
+        this.ob1 = ob1;
+        this.ob2 = ob2;
+    }
+
+    public T getOb1() {
+        return ob1;
+    }
+
+    public V getOb2() {
+        return ob2;
+    }
+
+    public void showTypes() {
+        System.out.println("ob1: " + ob1.getClass().getName());
+        System.out.println("ob2: " + ob2.getClass().getName());
+    }
+}
+
+public class TwoGenDemo {
+    public static void main(String[] args) {
+        TwoGen<Integer, String> twoGen = new TwoGen<>(27, "Hello");
+        twoGen.showTypes();
+    }
+}
