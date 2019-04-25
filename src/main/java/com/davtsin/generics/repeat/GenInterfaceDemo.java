2b44f9585614dadbf040adef4e66b76baf087b1a
Avtsin Denis <avtsin@ofd.ru>
2019-04-25 12:20:10 +0300

    generics: interface demo

---
+++ src/main/java/com/davtsin/generics/repeat/GenInterfaceDemo.java
@@ -0,0 +1,39 @@
+package com.davtsin.generics.repeat;
+
+
+interface Pair<K, V> {
+    K getKey();
+
+    V getValue();
+}
+
+
+class OrderedPair<K, V> implements Pair<K, V> {
+
+    private K key;
+    private V value;
+
+    public OrderedPair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+}
+
+public class GenInterfaceDemo {
+    public static void main(String[] args) {
+        Pair<String, Integer> pair = new OrderedPair<>("Hello", 8);
+
+        System.out.println(pair.getKey());
+        System.out.println(pair.getValue());
+    }
+}
