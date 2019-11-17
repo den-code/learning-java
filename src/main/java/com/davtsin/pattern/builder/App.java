74425b00b915709e210bf45ac77105a2388f6c22
denis <davtsin@gmail.com>
2019-11-17 10:07:40 +0300

    pattern: builder - java 11 improvement

--- src/main/java/com/davtsin/pattern/builder/App.java
+++ src/main/java/com/davtsin/pattern/builder/App.java
@@ -7,14 +7,14 @@ public class App {
     private static Logger logger = LoggerFactory.getLogger(com.davtsin.pattern.abstractfactory.App.class);
 
     public static void main(String[] args) {
-        Hero thief = new Hero.Builder("Thief", Profession.THIEF)
+        var thief = new Hero.Builder("Thief", Profession.THIEF)
                 .withWeapon(Weapon.KNIFE)
                 .withHairColour(HairColour.WHITE)
                 .build();
 
         logger.info(thief.toString());
 
-        Hero robber = new Hero.Builder("Robber", Profession.ROBBER)
+        var robber = new Hero.Builder("Robber", Profession.ROBBER)
                 .withArmour(Armour.KEVLAR_JACKET)
                 .withHairColour(HairColour.BLACK)
                 .withWeapon(Weapon.GUN)
